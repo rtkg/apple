ae2656f7a249a36c81ae365364c0d1750ec1109c
Robert Krug <robert.krug@oru.se>
2015-01-11 15:00:48 +0100

    some cosmetic changes

--- hqp_controllers/src/hqp_velocity_controller.cpp
+++ hqp_controllers/src/hqp_velocity_controller.cpp
@@ -1,40 +1,12 @@
-#include <controller_interface/controller.h>
-#include <hardware_interface/joint_command_interface.h>
-#include <pluginlib/class_list_macros.h>
 #include <hqp_controllers/hqp_velocity_controller.h>
+#include <pluginlib/class_list_macros.h>
 
-namespace controller_ns{
-
-class PositionController : public controller_interface::Controller<hardware_interface::EffortJointInterface>
+template <class T>
+void forward_command_controller::ForwardCommandController<T>::starting(const ros::Time& time)
 {
-public:
-  bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n)
-  {
-    // get joint name from the parameter server
-    std::string my_joint;
-    if (!n.getParam("joint", my_joint)){
-      ROS_ERROR("Could not find joint name");
-      return false;
-    }
+  // Start controller with 0.0 effort
+  command_ = 0.0;
+}
 
-    // get the joint object to use in the realtime loop
-    joint_ = hw->getHandle(my_joint);  // throws on failure
-    return true;
-  }
 
-  void update(const ros::Time& time, const ros::Duration& period)
-  {
-    double error = setpoint_ - joint_.getPosition();
-    joint_.setCommand(error*gain_);
-  }
-
-  void starting(const ros::Time& time) { }
-  void stopping(const ros::Time& time) { }
-
-private:
-  hardware_interface::JointHandle joint_;
-  static const double gain_ = 1.25;
-  static const double setpoint_ = 3.00;
-};
-PLUGINLIB_DECLARE_CLASS(package_name, PositionController, controller_ns::PositionController, controller_interface::ControllerBase);
-}//namespace
+PLUGINLIB_EXPORT_CLASS(effort_controllers::JointEffortController,controller_interface::ControllerBase)
