0e5804fcab18352f4a039d75729ae6db6d4fb03c
Todor Stoyanov <todor.stoyanov@oru.se>
2015-02-04 16:10:51 +0100

    dummy demo node with condition variables

--- demo_palletizing/include/demo_palletizing/demo_palletizing.h
+++ demo_palletizing/include/demo_palletizing/demo_palletizing.h
@@ -5,6 +5,10 @@
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition_variable.hpp>
 
+#include <std_srvs/Empty.h>
+#include <hqp_controllers_msgs/TaskStatuses.h>
+#include <hqp_controllers_msgs/SetTasks.h>
+
 namespace demo_palletizing
 {
 class DemoPalletizing
@@ -16,13 +20,25 @@ public:
 private:
 
     ros::NodeHandle nh_;
+    ros::NodeHandle n_;
+    boost::mutex arm_tasks_m;
+    boost::condition_variable cond;
 
+    bool task_status_changed;
+    bool task_success;
 
+    ros::Subscriber taks_status_sub;
+    ros::ServiceClient send_arm_task;
+    ros::ServiceServer start_demo_srv;
+
+    std::string set_task_name, task_state_topic;
     /////////////////
     //  CALLBACKS  //
     /////////////////
+    void stateCallback( const hqp_controllers_msgs::TaskStatusesPtr& msg);
+    bool start_demo(std_srvs::Empty::Request  &req,
+	    std_srvs::Empty::Response &res );
 
-    // bool setTaskObjects(hqp_controllers_msgs::SetTaskObjects::Request & req, hqp_controllers_msgs::SetTaskObjects::Response &res);
 };
 
 }//end namespace hqp controllers
--- demo_palletizing/src/demo_palletizing.cpp
+++ demo_palletizing/src/demo_palletizing.cpp
@@ -6,9 +6,70 @@ namespace demo_palletizing
 DemoPalletizing::DemoPalletizing()
 {
 
+    //handle to home
+    nh_ = ros::NodeHandle("~");
+    //global handle
+    n_ = ros::NodeHandle();
+
+    //get params
+    nh_.param<std::string>("set_task_service", set_task_name,"/set_task");
+    nh_.param<std::string>("task_state_topic", task_state_topic,"/task_state");
+
+    //initialize variables
+    task_status_changed = false;
+    task_success = false;
+
+    //register callbacks
+    start_demo_srv = nh_.advertiseService("start_demo", &DemoPalletizing::start_demo, this);;
+    taks_status_sub = n_.subscribe(task_state_topic, 10, &DemoPalletizing::stateCallback, this);
+    send_arm_task = n_.serviceClient<hqp_controllers_msgs::SetTasks>(set_task_name);
 
 }
 //-----------------------------------------------------------------
+void DemoPalletizing::stateCallback( const hqp_controllers_msgs::TaskStatusesPtr& msg) {
+
+    //inside this block it is safe to modify task status and success
+    boost::mutex::scoped_lock lock(arm_tasks_m);
+    //here monitor specific task and once status changes do:
+    task_status_changed = true;
+    task_success = true;
+    cond.notify_one();
+}
+
+bool DemoPalletizing::start_demo(std_srvs::Empty::Request  &req,
+	std_srvs::Empty::Response &res ) {
+
+    //call some blocking service for moving the forklift to position 
+    
+    //add a task to the arm controller
+    {
+	ROS_INFO("entering the service lock");
+	//inside this scope {} it is safe to access shared data like task_status, task_success
+	boost::mutex::scoped_lock lock (arm_tasks_m);
+	/*
+	hqp_controllers_msgs::SetTasks task;
+	if(!send_arm_task.call(task)) {
+	    ROS_ERROR("could not send task to arm");
+	    //handle errors or
+	    ros::shutdown();
+	    return false;
+	}
+	*/
+	while(!task_status_changed) {
+	    cond.wait(lock);
+	}
+	if(!task_success) {
+	    //handle errors, recover or
+	    ros::shutdown();
+	    return false;
+	}
+	ROS_INFO("made it through the lock");
+    }
+    //here lock is released, so task status cannot be checked safely.
+    //you can add another block for the next task...
+
+    return true;
+}
 }//end namespace demo_palletizing
 
 
@@ -25,11 +86,10 @@ int main(int argc, char **argv)
     demo_palletizing::DemoPalletizing demo;
 
     ROS_INFO("Demo palletizing node ready");
-    while(ros::ok())
-    {
-        //do smthing
-        ros::spinOnce();
-    }
+    ros::AsyncSpinner spinner(4); // Use 4 threads
+    spinner.start();
+    ros::waitForShutdown();
+
     return 0;
 }
 //---------------------------------------------------------------------
